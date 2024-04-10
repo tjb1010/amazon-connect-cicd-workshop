feec0c05c3256c4b21031a3849d1d01ab00a5144
Tyler Bickford <>
2024-04-10 11:27:57 -0700

    pipeline test

--- amzconnect-lambdas/test/connect-lambda.test.ts
+++ amzconnect-lambdas/test/connect-lambda.test.ts
@@ -4,7 +4,7 @@
 
 // example test. To run these tests, uncomment this file along with the
 // example resource in lib/connect-cicd-stack.ts
-test('SQS Queue Created', () => {
+// test('SQS Queue Created', () => {
 //   const app = new cdk.App();
 //     // WHEN
 //   const stack = new ConnectCicd.ConnectCicdStack(app, 'MyTestStack');
